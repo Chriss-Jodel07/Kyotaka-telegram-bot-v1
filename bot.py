8fae4f97bdc2d3f2b194f0e44bb36ae3a7a6dd94
Chriss-Jodel07 <chrissjodel@gmail.com>
2025-07-19 07:29:14 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ from commands.nsfw import nsfw
 from commands.ai_kyo import ai_kyo
 from commands.ban import ban     
 
-TOKEN = "LE_TOKEN_DE_TON_BOT"
+TOKEN = "7811604761:AAGpVDfjyFz9nNCAS1GYe5dPNLUcxqDmB2Y"
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
